ac5ab46e4ca42e79929110b850d7188555befe3f
yacinej@outlook.fr <yacinej@outlook.fr>
2023-07-27 16:22:20 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let Apropos = document.querySelector("#Apropos");
+Apropos.addEventListener("mouseover", () => {
+    Apropos.style.backgroundColor = "lightblue";
+});
+Apropos.addEventListener("mouseout", () => {
+    Apropos.style.backgroundColor = "";
+});
+
+let Experiences = document.querySelector("#Experiences");
+Experiences.addEventListener("mouseover", () => {
+    Experiences.style.backgroundColor = "lightblue";
+});
+Experiences.addEventListener("mouseout", () => {
+    Experiences.style.backgroundColor = "";
+});
+
+let Projets = document.querySelector("#Projets");
+Projets.addEventListener("mouseover", () => {
+    Projets.style.backgroundColor = "lightblue";
+});
+Projets.addEventListener("mouseout", () => {
+    Projets.style.backgroundColor = "";
+});
