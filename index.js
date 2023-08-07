26697617e05c01c701647c1689ffff1594f4c126
Yacine-J <yacinej@outlook.fr>
2023-08-07 21:59:08 +0200

    initial commit

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ Apropos.addEventListener("mouseout", () => {
     Apropos.style.backgroundColor = "";
 });
 
-function toggleMenu() {
-    let menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
-  }
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
